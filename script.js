8cc29a5e18aaea8cb2fb96cf86bb54def28f7e73
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-02-15 13:48:14 +0530

    test

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 let input = document.getElementById("contentinput");
+
+let json = {
+  rid:123559489,
+  status:"WIP",
+  current:"Pickling",
+  length:123,
+  width:155,
+  thickness:22,
+  remarks:"some stuff written by some one"
+}
+
+let jsonText = JSON.stringify(json);
+console.log(jsonText);
+console.log(JSON.parse(jsonText));
+
 document.getElementById("generate").addEventListener("click",()=>{
   let canvas = document.createElement('canvas');
   let options = {
     bcid        : 'qrcode',       // Barcode type
     text        : input.value,    // Text to encode
-    scale       : 3,               // 3x scaling factor
-    // height      : auto,              // Bar height, in millimeters
+    scale       : 1,               // 1x scaling factor
+    height      : 150,              // Bar height, in millimeters
+    width       : 150,
     includetext : true,            // Show human-readable text
     textxalign  : 'center',        // Always good to set this
+    backgroundcolor:'red'
   }
   try{
     bwipjs.toCanvas(canvas,options);
     document.getElementById('imgqr').src = canvas.toDataURL('image/png');
     document.getElementById('download').href = canvas.toDataURL('image/png');
   }catch(e){
-  
+    console.error(e)
   }
 })
